25f11c2c1973e0cb400980af035c62f039cc7178
sylwia <gydiihso@gmail.com>
2022-02-15 13:30:37 +0100

    updated hello page

--- src/main/java/com/example/demo/HelloPage.java
+++ src/main/java/com/example/demo/HelloPage.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class HelloPage {
-    @GetMapping("/hello/Mark")
-    public String hello() {
-        return "Hello Mark";
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable("name") String name) {
+
+        return "Hello " + name;
     }
 }
